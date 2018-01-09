25c2e6e92844a06702b06aa067f1db6bfad8fe88
Alex Rossi Ignez <rx.rossi@gmail.com>
2018-01-09 02:23:01 +0000

    Clients now sends id and _id fields

--- src/models/Clients.js
+++ src/models/Clients.js
@@ -5,9 +5,12 @@ const clientsSchema = mongoose.Schema({
   phone: String,
 });
 
-clientsSchema.virtual('id').get(function() {
-    return this._id.toHexString();
+clientsSchema.virtual('id').get(function () {
+  return this._id.toHexString();
 });
+
+clientsSchema.set('toObject', { getters: true, virtuals: true });
+clientsSchema.set('toJSON', { getters: true, virtuals: true });
 
 export default mongoose.model('Client', clientsSchema);
 
