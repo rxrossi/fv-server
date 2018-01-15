49a61975b8a0c5c349b482153bd40989b377428f
rxrossi <rx.rossi@gmail.com>
2018-01-15 15:46:09 +0000

    Making Purchases tests cleaner, adding update purchase test

--- src/controllers/Purchases.test.js
+++ src/controllers/Purchases.test.js
@@ -1,99 +1,45 @@
 import mongoose from 'mongoose';
-import ProductsController from './Products';
-import StockController from './Stock';
 import PurchasesController from './Purchases';
 import ProductModel from '../models/Products';
 import StockModel from '../models/Stock';
+import ProductsController from './Products';
 import PurchasesModel from '../models/Purchases';
 
-describe('Purchases Controller', () => {
-  let sut;
+const productsController = new ProductsController();
+const sut = new PurchasesController();
 
+const errLogger = (err) => {
+  if (err) {
+    // eslint-disable-next-line
+    console.error(err);
+  }
+};
+
+const cleanUpDB = () =>
+  ProductModel.deleteMany({}, errLogger)
+    .then(() => StockModel.deleteMany({}, errLogger))
+    .then(() => PurchasesModel.deleteMany({}), errLogger);
+
+describe('Purchases Controller', () => {
   beforeEach((done) => {
     mongoose.Promise = global.Promise;
     mongoose.connect('mongodb://localhost/fv', { useMongoClient: true })
+      .then(() => cleanUpDB())
       .then(() => done());
   });
 
-  beforeEach((done) => {
-    // Delete all products
-    ProductModel.deleteMany({}, (err) => {
-      if (err) {
-        console.error('an error', err);
-      }
-      done();
-    });
-  });
-
-  afterEach((done) => {
-    // Delete all products
-    ProductModel.deleteMany({}, (err) => {
-      if (err) {
-        console.error('an error', err);
-      }
-      done();
-    });
-  });
-
-  beforeEach((done) => {
-    // Delete all stock
-    StockModel.deleteMany({}, (err) => {
-      if (err) {
-        console.error('an error', err);
-      }
-      done();
-    });
-  });
-
-  afterEach((done) => {
-    // Delete all stock
-    StockModel.deleteMany({}, (err) => {
-      if (err) {
-        console.error('an error', err);
-      }
-      done();
-    });
-  });
-
-  beforeEach((done) => {
-    // Delete all purchases
-    PurchasesModel.deleteMany({}, (err) => {
-      if (err) {
-        console.error('an error', err);
-      }
-      done();
-    });
-  });
-
-  afterEach((done) => {
-    // Delete all purchases
-    PurchasesModel.deleteMany({}, (err) => {
-      if (err) {
-        console.error('an error', err);
-      }
-      done();
-    });
-  });
 
   describe('With no entries yet', () => {
     let ox;
     let shampoo;
-    beforeEach((done) => {
-      ox = new ProductModel({ name: 'OX', measure_unit: 'ml' });
-      ox.save((err, product) => {
-        if (err) {
-          console.error(err);
-        }
-        done();
-      });
+    beforeEach(async (done) => {
+      ox = await productsController.create({ name: 'ox', measure_unit: 'ml' })
+        .then(({ product }) => product);
 
-      shampoo = new ProductModel({ name: 'shampoo', measure_unit: 'ml' });
-      shampoo.save((err, product) => {
-        if (err) {
-          console.error(err);
-        }
-        done();
-      });
+      shampoo = await productsController.create({ name: 'shampoo', measure_unit: 'ml' })
+        .then(({ product }) => product);
+
+      done();
     });
 
     it('correctly posts a new purchase', async () => {
@@ -106,7 +52,6 @@ describe('Purchases Controller', () => {
         seller: 'Company one',
         date: Date.now(),
       };
-      const sut = new PurchasesController();
 
       // Act
       await sut.create(postBody);
@@ -130,7 +75,6 @@ describe('Purchases Controller', () => {
         date: Date.now(),
       };
 
-      const sut = new PurchasesController();
       await sut.create(postBody);
 
       // Act
@@ -138,6 +82,53 @@ describe('Purchases Controller', () => {
 
       // Assert
       expect(purchases[0].stockEntries[0].product.name).toEqual(ox.name);
+    });
+  });
+
+  describe('With a default entry', () => {
+    let ox;
+    let shampoo;
+    let cape;
+    let purchaseExample;
+    beforeEach(async (done) => {
+      // create shampoo and ox products
+      ox = await productsController.create({ name: 'ox', measure_unit: 'ml' })
+        .then(({ product }) => product);
+
+      shampoo = await productsController.create({ name: 'shampoo', measure_unit: 'ml' })
+        .then(({ product }) => product);
+
+      cape = await productsController.create({ name: 'cape', measure_unit: 'unit' })
+        .then(({ product }) => product);
+
+      // create a purchase example
+      const postBody = {
+        products: [
+          { id: ox._id, qty: 500, total_price: 90 },
+          { id: shampoo._id, qty: 1000, total_price: 40 },
+        ],
+        seller: 'Company one',
+        date: Date.now(),
+      };
+      purchaseExample = await sut.create(postBody);
+      done();
+    });
+
+    it('correctly update a sale', async () => {
+      // Prepare
+      const putBody = {
+        ...purchaseExample,
+        seller: 'Company two',
+        products: [
+          { id: shampoo._id, qty: 1000, total_price: 40 },
+          { id: cape._id, qty: 1, total_price: 1 },
+        ],
+      };
+      // Act
+      const updatedPurchase = await sut.update(putBody);
+
+      // Assert
+      expect(updatedPurchase).toMatchObject(putBody);
     });
   });
 });
